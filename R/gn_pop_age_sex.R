555f7f4617e84c9602bf45a37fc228ae359cde10
amalirajapaksha <amali2022ugc@gmail.com>
2026-06-25 14:55:24 +0530

    updated

--- R/gn_pop_age_sex.R
+++ R/gn_pop_age_sex.R
@@ -1,9 +1,13 @@
-#' Population by Sex and Age According to Districts
+#' Population by Sex and Age According to Grama Niladhari Division
 #'
-#' Census of Population and Housing 2024.
+#' Population counts by sex and age group at the Grama Niladhari (GN) level
+#' from the Sri Lanka Census of Population and Housing 2024.
+#'
 #'
 #' @format A data frame.
 #'
 #' @source Department of Census and Statistics Sri Lanka
+#' Census of Population and Housing 2024.
+#'
 "gn_pop_age_sex"
 
